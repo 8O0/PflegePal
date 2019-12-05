736a0293d4f96af8286ea3b19a4849e836581957
LukasBFH <loosl1@bfh.ch>
2019-12-05 14:03:05 +0100

    Had to change from FXMLoader to URL loader because of my ubuntu system

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,17 +1,23 @@
 package Main;
 	
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+
+import java.io.File;
+import java.net.URL;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = FXMLLoader.load(getClass().getResource("../view/MainView.fxml"));
+			URL url = new File("/home/abel/IdeaProjects/btx8052-mini-project/src/View/MainView.fxml").toURI().toURL();
+			Parent root = FXMLLoader.load(url);
+			//BorderPane root = FXMLLoader.load(getClass().getClassLoader().getResource());
 			Scene scene = new Scene(root,800,600);
 			primaryStage.setTitle("Medication Plan");
 			primaryStage.setScene(scene);
