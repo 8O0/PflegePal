acda0a4ab51f2d361959e871a71d2328976f6902
norman <norman@localhost>
2019-12-05 11:17:03 +0100

    Loading Vies ¨w

--- src/Controller/MainViewController.java
+++ src/Controller/MainViewController.java
@@ -1,8 +1,17 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
 import Model.Patient;
 
 public class MainViewController {
@@ -25,6 +34,24 @@ public class MainViewController {
 	public ListView<Patient> patientView;
 	
 	
+	
+	public void handleWeekPlanButton() {
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/WeekPlanView.fxml"));
+		Parent root1;
+		try {
+			root1 = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+			
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.initStyle(StageStyle.UNDECORATED);
+			stage.setScene(new Scene(root1));
+			stage.show();
+		} catch (IOException e) {
+			System.err.println("Error");
+		}
+				
+	}
 	
 	
 
