fed760d9db375ae2655b3ea16336708d284d9283
norman <norman@localhost>
2019-12-05 10:49:31 +0100

    Main View Controller

--- src/Controller/MainViewController.java
+++ src/Controller/MainViewController.java
@@ -1,5 +1,31 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import Model.Patient;
+
 public class MainViewController {
+	
+	
+	@FXML
+	public Button medicationbutton;
+	
+	
+	@FXML 
+	public Button weeklyplanbutton;
+	
+	@FXML
+	public Button importbutton;
+	
+	@FXML
+	public Button exportbutton;
+	
+	@FXML
+	public ListView<Patient> patientView;
+	
+	
+	
+	
 
 }
