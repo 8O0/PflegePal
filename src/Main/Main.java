47ccd940b64e22473efa09793579f38149dc1004
norman <norman@localhost>
2019-12-05 10:34:38 +0100

    Push

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,8 @@
 package Main;
 	
-import java.awt.MenuBar;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -14,19 +11,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
-			MenuBar menuBar = new MenuBar();
-			final String os = System.getProperty("os.name");
-			if (os != null && os.startsWith("Mac"));
-			
-			final Menu menu1 = new Menu("File");
-		    final Menu menu2 = new Menu("Options");
-		    final Menu menu3 = new Menu("Help");
-		   			
-			
-			
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../View/MainView.fxml"));
-			Scene scene = new Scene(root,400,400);
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../view/MainView.fxml"));
+			Scene scene = new Scene(root,800,600);
+			primaryStage.setTitle("Medication Plan");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
