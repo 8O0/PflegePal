7ac3e549dd50d6b3e7fd1c169a9e2c690a425336
norman <norman@localhost>
2019-12-05 08:40:56 +0100

    Project Setup

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,11 @@
 package Main;
 	
+import java.awt.MenuBar;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,6 +14,17 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
+			MenuBar menuBar = new MenuBar();
+			final String os = System.getProperty("os.name");
+			if (os != null && os.startsWith("Mac"));
+			
+			final Menu menu1 = new Menu("File");
+		    final Menu menu2 = new Menu("Options");
+		    final Menu menu3 = new Menu("Help");
+		   			
+			
+			
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../View/MainView.fxml"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
