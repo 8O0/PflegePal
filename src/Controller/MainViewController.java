4fce87107f3e47c5ef843097f1b0317b96e451d8
LukasBFH <loosl1@bfh.ch>
2019-12-12 08:47:06 +0100

    Had to change from FXMLoader to URL loader because of my ubuntu system

--- src/Controller/MainViewController.java
+++ src/Controller/MainViewController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.Patient;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,68 +9,63 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-import Model.Patient;
-
 public class MainViewController {
-	
-	
-	@FXML
-	public Button medicationbutton;
-	
-	
-	@FXML 
-	public Button weeklyplanbutton;
-	
-	@FXML
-	public Button importbutton;
-	
-	@FXML
-	public Button exportbutton;
-	
-	@FXML
-	public ListView<Patient> patientView;
-	
-	
-	public void handleMedicationButton() {
-		
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/MedicationView.fxml"));
-		Parent root2;
-		try {
-			root2 = (Parent) fxmlLoader.load();
-			Stage stage = new Stage();
-			
-			stage.initModality(Modality.APPLICATION_MODAL);
-			stage.setScene(new Scene(root2));
-			stage.show();
-		} catch (IOException e) {
-			System.err.println("Error"+e.getMessage());
-		}
-		
-	}
-	
-	
-	
-	public void handleWeekPlanButton() {
-		
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/WeekPlanView.fxml"));
-		Parent root1;
-		try {
-			root1 = (Parent) fxmlLoader.load();
-			Stage stage = new Stage();
-			
-			stage.initModality(Modality.APPLICATION_MODAL);
-			stage.setScene(new Scene(root1));
-			stage.show();
-		} catch (IOException e) {
-			System.err.println("Error");
-		}
-				
-	}
-	
-	
+
+
+    @FXML
+    public Button medicationbutton;
+
+
+    @FXML
+    public Button weeklyplanbutton;
+
+    @FXML
+    public Button importbutton;
+
+    @FXML
+    public Button exportbutton;
+
+    @FXML
+    public ListView<Patient> patientView;
+
+
+    public void handleMedicationButton() {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/MedicationView.fxml"));
+        Parent root2;
+        try {
+            root2 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root2));
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Error" + e.getMessage());
+        }
+
+    }
+
+
+    public void handleWeekPlanButton() {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/WeekPlanView.fxml"));
+        Parent root1;
+        try {
+            root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Error");
+        }
+
+    }
+
 
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,33 +1,27 @@
 package Main;
-	
+
 import javafx.application.Application;
-import javafx.scene.Parent;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
-
-import java.io.File;
-import java.net.URL;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			//URL url = new File("/home/abel/IdeaProjects/btx8052-mini-project/src/View/MainView.fxml").toURI().toURL();
-			//Parent root = FXMLLoader.load(url);
-			BorderPane root = FXMLLoader.load(getClass().getResource("../View/MainView.fxml"));
-			Scene scene = new Scene(root,800,600);
-			primaryStage.setTitle("Medication Plan");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+
+            BorderPane root = FXMLLoader.load(getClass().getResource("../View/MainView.fxml"));
+            Scene scene = new Scene(root, 800, 600);
+            primaryStage.setTitle("Medication Plan");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
