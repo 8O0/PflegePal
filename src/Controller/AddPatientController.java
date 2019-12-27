be50fff97ed7a9ae0e692fa6faf9d40ca63ab31a
Lukas <lukas.loosli@student.bfh.ch>
2019-12-27 17:43:42 +0100

    Prescribe works Cleanup of code Added UmlDiagramm.uml Added Gender and Address to Person

--- src/Controller/AddPatientController.java
+++ src/Controller/AddPatientController.java
@@ -8,13 +8,15 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import static java.lang.Boolean.TRUE;
+
 public class AddPatientController {
 
     @FXML
-    public Button submitButton,cancelButton;
+    public Button submitButton, cancelButton;
 
     @FXML
-    public TextField firstname,lastname,age,illness, address;
+    public TextField firstname, lastname, age, illness, address;
 
     @FXML
     public RadioButton maleRadioButton, femaleRadioButton;
@@ -36,12 +38,21 @@ public class AddPatientController {
 
     @FXML
     private void handleSubmit() {
-        Patient patient = new Patient(firstname.getText(), lastname.getText(),Integer.valueOf(age.getText()).intValue(), illness.getText(), address.getText(), (maleRadioButton.isArmed()) ? "M" :  "F");
+       //String gender = isGender(maleRadioButton.isArmed(), femaleRadioButton.isArmed());
+        Patient patient = new Patient(firstname.getText(), lastname.getText(), Integer.valueOf(age.getText()).intValue(), (isGender(maleRadioButton.isSelected(), femaleRadioButton.isSelected())), address.getText(), illness.getText());
         model.addPatient(patient);
         Stage stage = (Stage) submitButton.getScene().getWindow();
         stage.close();
     }
 
+    private String isGender(boolean isMale, boolean isFemale){
+        if (isMale==TRUE)
+            return "Male";
+        else if(isFemale==TRUE)
+            return "Female";
+        else return "Not disclosed";
+        }
+    }
     /*
     @FXML
     private void handleSubmit() {
@@ -50,5 +61,5 @@ public class AddPatientController {
         Stage stage = (Stage) submitButton.getScene().getWindow();
         stage.close();
     }
+    }
 */
-}
--- src/Model/Patient.java
+++ src/Model/Patient.java
@@ -6,16 +6,11 @@ public class Patient extends Person {
 
     public static Function<String, Patient> mapToPatient = (line) -> {
         String[] p = line.split(", ");
-        return new Patient(p[0], p[1], Integer.parseInt(p[2]), p[3],p[4],p[5]);
+        return new Patient(p[0], p[1], Integer.parseInt(p[2]), p[3], p[4], p[5]);
     };
 
     private String illness;
-/*
-    public Patient(String surname, String name, int age, String illness) {
-        super(name, surname, age);
-        this.illness = illness;
-    }
-*/
+
     public Patient(String name, String surname) {
         super(name, surname);
     }
